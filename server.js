4af87b3373bba24099471b27551ac489317fd988
Adnan Fazil <danifazil99@gmail.com>
2025-03-24 14:14:18 +0500

    New api added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 //const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello Waleed');
+});
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
