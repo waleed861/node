e4c9bc23113d581c7e1aeaf5b6ddd2d1beb0d905
waleed <waleedurrehman861@gmail.com>
2025-03-24 14:06:53 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+//const PORT = 3000;
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
